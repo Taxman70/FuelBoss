7d4f1a803642b2cc765f0084692ebb742c5bfe3e
Thomas Bennedum <tab@bennedum.org>
2019-01-02 15:16:09 -0500

    First cut of code

--- arduino/README.md
+++ arduino/README.md
@@ -1,2 +1,2 @@
 # arduino
-Firmware for ...
+Firmware for Fuel Boss
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,16 +2,34 @@
 #include <Arduino.h>
 #include <EEPROM.h>
 
-constexpr unsigned long SERIAL_SPEED    = 115200;
+constexpr uint32_t  SERIAL_SPEED    = 115200;
 
 constexpr int PIN_SERIAL_RX             = 0;
 constexpr int PIN_SERIAL_TX             = 1;
 
 constexpr int PIN_LED                   = 13;
+constexpr int PIN_MAINTANK_TRIGGER      = 4;
+constexpr int PIN_MAINTANK_READ         = 3;
+constexpr int PIN_AUXTANK_READ          = A0;
 
-constexpr int INPUT_BUFFER_LENGTH       = 32;
+constexpr uint8_t INPUT_BUFFER_LENGTH   = 32;
+constexpr uint8_t RING_BUFFER_LENGTH    = 16;
 
-constexpr unsigned long LED_TOGGLE_INTERVAL = 500;
+constexpr uint32_t  LED_TOGGLE_INTERVAL = 500;
+constexpr uint32_t  MAINTANK_INTERVAL   = 100;
+constexpr uint32_t  AUXTANK_INTERVAL    = 1000;
+constexpr uint32_t  SEND_INTERVAL       = 60000;
+
+constexpr uint32_t  MAINTANK_REJECT     = 290;  // reject readings that are more than this many uS apart
+constexpr uint32_t  MAINTANK_TIMEOUT    = 1000; // timeout in uS for waiting for rising signal
+constexpr uint32_t  MAINTANK_CONVERT    = 58;   // cm = uS / MAINTANK_CONVERT
+
+constexpr uint32_t  AUXTANK_REJECT      = 10;   // reject readings that are more than this many apart
+
+constexpr uint8_t ST_IDLE       = 0;
+constexpr uint8_t ST_WAIT_RISE  = 1;
+constexpr uint8_t ST_WAIT_FALL  = 2;
+constexpr uint8_t ST_READ       = 3;
 
 
 typedef struct {
@@ -19,11 +37,31 @@ typedef struct {
     uint8_t length;
 } inputBuffer_t;
     
+typedef struct {
+    uint32_t data[RING_BUFFER_LENGTH];
+    uint8_t ptr;
+    bool looped;
+    uint32_t average;
+    bool calcedAverage;
+    uint32_t last;
+} ringBuffer_t;
+    
 inputBuffer_t inputBuffer;
 int inputCh;
 
 bool ledOn = false;
 uint32_t lastLEDToggleTime = 0;
+
+volatile uint8_t mainTankState = ST_IDLE;
+uint32_t lastMainTankReadTime = 0;
+volatile uint32_t mainTankPulseTime = 0;
+
+uint32_t lastAuxTankReadTime = 0;
+
+uint32_t lastSendTime = 0;
+
+ringBuffer_t mainTankBuffer;
+ringBuffer_t auxTankBuffer;
 
 
 void setup() {
@@ -31,14 +69,23 @@ void setup() {
 
     pinMode(PIN_LED, OUTPUT);
     
+    pinMode(PIN_MAINTANK_TRIGGER, OUTPUT);
+    pinMode(PIN_MAINTANK_READ, INPUT);
+    attachInterrupt(digitalPinToInterrupt(PIN_MAINTANK_READ), readMainTank, CHANGE);
+    
     turnOffLED();
-        
+
     sendMessage(F("Ready"));
 }
 
 void loop() {
     loopSerial();
     loopLED();
+    loopMainTank();
+    loopAuxTank();
+    loopSend();
+    
+    // TODO: add sensors detecting state of boiler/hot water heater?
 }
 
 void loopSerial() {
@@ -78,9 +125,67 @@ void loopLED() {
     }
 }
 
-void resetInputBuffer() {
-    inputBuffer.data[0] = '\0';
-    inputBuffer.length = 0;
+void loopMainTank() {
+    switch (mainTankState) {
+        case ST_IDLE:
+            if ((millis() - lastMainTankReadTime) >= MAINTANK_INTERVAL) {
+                lastMainTankReadTime = millis();
+                mainTankState = ST_WAIT_RISE;
+                digitalWrite(PIN_MAINTANK_TRIGGER, HIGH);
+                delayMicroseconds(10);
+                digitalWrite(PIN_MAINTANK_TRIGGER, LOW);
+            }
+            break;
+        case ST_WAIT_RISE:
+        case ST_WAIT_FALL:
+            if ((millis() - lastMainTankReadTime) >= MAINTANK_TIMEOUT) {
+                lastMainTankReadTime = millis();
+                mainTankState = ST_IDLE;
+                sendMessage(F("main tank timeout"));
+            }
+            break;
+        case ST_READ:
+            lastMainTankReadTime = millis();
+            send(F("#main tank: "));
+            sendLUInt(mainTankPulseTime);
+            sendChar('\n');
+            if ((mainTankPulseTime - ringBufferLast(&mainTankBuffer, mainTankPulseTime)) > MAINTANK_REJECT) {
+                sendMessage(F("main tank reject"));
+            } else {
+                ringBufferInsert(&mainTankBuffer, mainTankPulseTime);
+            }
+            mainTankState = ST_IDLE;
+            break;
+    }
+}
+
+void loopAuxTank() {
+    if ((millis() - lastAuxTankReadTime) >= AUXTANK_INTERVAL) {
+        lastAuxTankReadTime = millis();
+        int val = analogRead(PIN_AUXTANK_READ);
+        send(F("#aux tank: "));
+        sendInt(val);
+        sendChar('\n');
+        if ((val - ringBufferLast(&auxTankBuffer, val)) > AUXTANK_REJECT) {
+            sendMessage(F("aux tank reject"));
+        } else {
+            ringBufferInsert(&auxTankBuffer, val);
+        }
+    }
+}
+
+void loopSend() {
+    if ((millis() - lastSendTime) >= SEND_INTERVAL) {
+        lastSendTime = millis();
+        
+        send(F("*M"));
+        sendLUInt(ringBufferAverage(&mainTankBuffer));
+        sendChar('\n');
+        
+        send(F("*A"));
+        sendLUInt(ringBufferAverage(&auxTankBuffer));
+        sendChar('\n');
+    }
 }
 
 void processCommand() {
@@ -92,6 +197,16 @@ void processCommand() {
             if (! processChecksum(cmd, len)) break;
         case 'e':
             processEEPROMCommand(cmd + 1);
+            break;
+        case 'M':
+            if (! processChecksum(cmd, len)) break;
+        case 'm':
+            processMainTankCommand(cmd + 1);
+            break;
+        case 'A':
+            if (! processChecksum(cmd, len)) break;
+        case 'a':
+            processAuxTankCommand(cmd + 1);
             break;
         default:
             sendError(F("invalid command"));
@@ -141,8 +256,120 @@ void cmdEEPROMClear(char* str) {
     sendOK();
 }
 
+// =========== Main Tank commands
+
+void processMainTankCommand(char* cmd) {
+    switch (cmd[0]) {
+        case '?':
+            ringBufferStatus(&mainTankBuffer);
+            break;
+        default:
+            sendError(F("invalid main tank command"));
+            break;
+    }
+}
+
+// =========== Aux Tank commands
+
+void processAuxTankCommand(char* cmd) {
+    switch (cmd[0]) {
+        case '?':
+            ringBufferStatus(&auxTankBuffer);
+            break;
+        default:
+            sendError(F("invalid aux tank command"));
+            break;
+    }
+}
+
+// =========== Interrupt Service Routine
+
+void readMainTank() {
+    switch (mainTankState) {
+        case ST_WAIT_RISE:
+            mainTankPulseTime = micros();
+            mainTankState = ST_WAIT_FALL;
+            break;
+        case ST_WAIT_FALL:
+            mainTankPulseTime = micros() - mainTankPulseTime;
+            mainTankState = ST_READ;
+            break;
+    }
+}
 
 // =========== Other stuff
+
+void resetInputBuffer() {
+    inputBuffer.data[0] = '\0';
+    inputBuffer.length = 0;
+}
+
+uint32_t ringBufferLast(const ringBuffer_t* buffer, uint32_t def) {
+    if (buffer->looped || buffer->ptr) {
+        return buffer->last;
+    } else {
+        return def;
+    }
+}
+
+uint32_t ringBufferAverage(ringBuffer_t* buffer) {
+    if (buffer->calcedAverage) {
+        return buffer->average;
+    } else if (buffer->looped) {
+        uint32_t avg;
+        for (int i = 0; i < RING_BUFFER_LENGTH; i++)
+            avg += buffer->data[i];
+        avg = avg / RING_BUFFER_LENGTH;
+        buffer->average = avg;
+        buffer->calcedAverage = true;
+    } else if (buffer->ptr) {
+        uint32_t avg;
+        for (int i = 0; i < buffer->ptr; i++)
+            avg += buffer->data[i];
+        avg = avg / buffer->ptr;
+        buffer->average = avg;
+        buffer->calcedAverage = true;
+    } else {
+        return 0;
+    }
+}
+
+void ringBufferInsert(ringBuffer_t* buffer, uint32_t val) {
+    buffer->last = val;
+    buffer->calcedAverage = false;
+    buffer->data[buffer->ptr] = val;
+    buffer->ptr = (buffer->ptr + 1) % RING_BUFFER_LENGTH;
+    if (buffer->ptr == 0)
+        buffer->looped = true;
+}
+
+void ringBufferStatus(const ringBuffer_t* buffer) {
+    for (int i = 0; i < RING_BUFFER_LENGTH; i++) {
+        send(F("buf "));
+        sendInt(i);
+        send(F(": "));
+        sendLUInt(buffer->data[i]);
+        if (buffer->ptr == i)
+            send(F(" <-"));
+        sendChar('\n');
+    }
+    send(F("looped: "));
+    sendInt(buffer->looped);
+    sendChar('\n');
+    
+    send(F("average: "));
+    sendLUInt(buffer->average);
+    sendChar('\n');
+    
+    send(F("calcedAverage: "));
+    sendInt(buffer->calcedAverage);
+    sendChar('\n');
+    
+    send(F("last: "));
+    sendLUInt(buffer->last);
+    sendChar('\n');
+}
+
 
 int readInt(char** strPtr) {
     bool neg = false;
